789b9b91ec7788e392cedd3801e2aecee4f47936
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-03-15 19:02:08 +0530

    fixed bug in generate matrix

--- generate_matrix.py
+++ generate_matrix.py
@@ -8,19 +8,24 @@ from dot_product_logic import dot_product
 with open('./data/grouped_and_no_duplicates.json','r') as f:
     data = json.load(f)
 
-myPreference = [{'name':'atm','weight':2}, {'name':'school','weight':1}]
+myPreference = [
+    {'name':'atm','weight':2},
+    {'name':'school','weight':1}
+    ]
 
 heatmaps = []
 for lat in data:
     for lng in data[lat]:
-        names = []
+        details = []
         heat = 0
         for establishment_id in data[lat][lng]:
             establishment = data[lat][lng][establishment_id]
-            heat += dot_product(myPreference, establishment)
-            names.append(establishment['name'])
-        if not len(names) == 0:
-            heatmaps.append({'name':names,
+            establishment_heat = dot_product(myPreference, establishment)
+            if establishment_heat >= 0.1:
+                heat += establishment_heat
+                details.append(establishment)
+        if not len(details) == 0:
+            heatmaps.append({'details':details,
                              'lat':lat,
                              'lng':lng,
                              'heat':heat})
